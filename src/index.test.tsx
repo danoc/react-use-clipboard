3b814e58e4d21e31b9d58c03498bbc8bd8843d04
Daniel O'Connor <daniel@danoc.me>
2019-08-11 14:04:29 -0700

    Add `successDuration` option for success message.
    This fixes #4.

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { cleanup, fireEvent, render } from "@testing-library/react";
+import { cleanup, fireEvent, render, act } from "@testing-library/react";
 import useClipboard from "./";
 
 afterEach(cleanup);
@@ -23,4 +23,72 @@ test("display sucess message if the copy worked", () => {
   fireEvent.click(button);
 
   expect(button.textContent).toBe("Yes");
+});
+
+describe("successDuration", () => {
+  test("`isCopied` becomes false after `successDuration` time ellapses", () => {
+    jest.useFakeTimers();
+    const successDuration = 1000;
+
+    const Component = () => {
+      const [isCopied, setCopied] = useClipboard("Text to copy", {
+        successDuration
+      });
+
+      return (
+        <button onClick={setCopied} data-testid="btn-example">
+          {isCopied ? "Yes" : "Nope"}
+        </button>
+      );
+    };
+
+    const { getByTestId } = render(<Component />);
+    const button = getByTestId("btn-example");
+
+    expect(button.textContent).toBe("Nope");
+
+    fireEvent.click(button);
+
+    expect(button.textContent).toBe("Yes");
+
+    // Fast-forward half-way
+    jest.advanceTimersByTime(successDuration / 2);
+
+    expect(button.textContent).toBe("Yes");
+
+    act(() => {
+      // Go to the end
+      jest.advanceTimersByTime(successDuration / 2);
+    });
+
+    expect(button.textContent).toBe("Nope");
+  });
+
+  test("`isCopied` is always true if `successDuration` is not provided", () => {
+    jest.useFakeTimers();
+
+    const Component = () => {
+      const [isCopied, setCopied] = useClipboard("Text to copy", {});
+
+      return (
+        <button onClick={setCopied} data-testid="btn-example">
+          {isCopied ? "Yes" : "Nope"}
+        </button>
+      );
+    };
+
+    const { getByTestId } = render(<Component />);
+    const button = getByTestId("btn-example");
+
+    expect(button.textContent).toBe("Nope");
+
+    fireEvent.click(button);
+
+    expect(button.textContent).toBe("Yes");
+
+    // Fast-forward half-way
+    jest.runAllTimers();
+
+    expect(button.textContent).toBe("Yes");
+  });
 });
